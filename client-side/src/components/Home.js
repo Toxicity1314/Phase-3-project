1cf2cd2f8d4d192bed6681afffaa256c2a019c49
Eddie Meyer <edmeyer8851@gmail.com>
2023-01-25 15:44:29 -0500

    added search functionality

--- client-side/src/components/Home.js
+++ client-side/src/components/Home.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import RestaurantCard from "./RestaurantCard";
-import { Pagination } from "semantic-ui-react";
+import { Pagination, Form, Button } from "semantic-ui-react";
 import { fetchHandler } from "../fetch";
 
 
 function Home({handleNavBar}) {
   const { pageNumber } = useParams(0);
-  console.log(pageNumber)
   
   const [restaurants, setRestaurants] = useState([]);
   const [activePage, setActivePage] = useState(1)
   const [sort, setSort] = useState("none")
+  const [searchField, setSearchField] = useState("")
 
   const handlePaginationChange = (e, {activePage})=>{
     setActivePage(activePage)
@@ -24,15 +24,33 @@ function Home({handleNavBar}) {
     return <RestaurantCard handleNavBar={handleNavBar} key={restaurant.id} restaurant={restaurant} />;
   });
 
-  console.log(restaurantCards);
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setActivePage(1)
+    fetchHandler(`restaurants/searchQuery/${searchField}/1/${sort}`, setRestaurants)
+    setSearchField("")
+  }
+
+  const handleChange = (e) => {
+    setSearchField(e.target.value)
+  }
 
   return (
     <div className="ui container center aligned">
       <br/>
       <p>Please select where you will be dining to view menus.</p>
-      <label>Choose a sort</label>
+      <Form onSubmit={handleSubmit}>
+        <Form.Field>
+            <label>Search</label>
+            <input name="search" onChange={handleChange} value={searchField} />
+        </Form.Field>
+        <Button type="submit">Search</Button>
+      </Form>
+      <br/>
+      <br/>
+      <label>Choose a sort: </label>
             <select name="food_course" onChange={(e)=> setSort(e.target.value)}>
-                <option value="none">--Please choose a sort: </option>
+                <option value="none">--Please choose a sort--</option>
                 <option value="ASC">A-Z</option>
                 <option value="DESC">Z-A</option>
             </select>
